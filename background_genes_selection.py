bd66590fc0ff7ec2e2b475c78cf533af24e2840e
Allen Lynch <alynch@dsalynchmacport.local>
2020-08-06 14:10:00 -0400

    no more pandas

--- background_genes_selection.py
+++ background_genes_selection.py
@@ -1,41 +1,45 @@
 
 import numpy as np
-import pandas as pd
 import os
 import random
+from collections import Counter, defaultdict
 
-#ADD DUPLICATE CHECKING 
+#ADD DUPLICATE CHECKING
 
 #enforce sampling according to the TAD distribution of the genome.
 def sampling_by_tad(background_gene_list, TAD_data, num_selected=3000):
     '''
     background_gene_list: list, genes from user-supplied list of background candidates, or genes that are background candidates because they are not in query
-    TAD_data: TAD cluster and regulatory group for each gene
+    TAD_data: [[GeneName, TadGroup], ...], TadGroup is a distinct regulatory state defined by the TAD and histone modification class
     num_selected: number of genes to sample for background
     '''
     assert(num_selected < len(TAD_data)), 'Number of genes to sample must be less than the number of background gene candidates'
 
     #intersect background gene list with TAD_list to eliminate reserved genes
-    TAD_data = TAD_data[TAD_data.geneName.isin(background_gene_list)]
-    
-    #create bins for distinct regulatory states (combination of TAD and methylation states)
-    TAD_data['bin'] = TAD_data.k4me3_order_cluster + TAD_data.tad_order_cluster
+    TAD_data = [(gene_name, tad_group) for (gene_name, tad_group) in TAD_data if gene_name in background_gene_list]
 
     #collect list of genes in each bin
-    aggregated_by_TAD = TAD_data.groupby('bin')['geneName'].apply(list).rename(columns = {'list' : 'genes_in_tad'})
-
-    #count number of genes in each bin
-    aggregated_by_TAD['num_genes_in_TAD'] = TAD_data.genes_in_tad.str.len()
+    genes_in_TAD = defaultdict(set)
+    for gene_name, tad_group in TAD_data:
+        genes_in_TAD[tad_group].append(gene_name)
+    
+    #calculates number of genes in each TAD group
+    num_genes_in_TAD = {tad_group : len(genes) for tad_group, genes in genes_in_TAD.items()}
 
     #calculates the number of genes expected to be sampled from each TAD, with a bit of an enrichment (1.1x). Ensure all TADs have >= 1 expected genes
-    aggregated_by_TAD['expected_samples'] = np.maximum(int(aggregated_by_TAD.num_genes_in_TAD / len(TAD_data) * num_selected * 1.1), 1.0)
+    expected_samples = {
+        tad_group : max(1, int(num_genes / len(TAD_data)) * num_selected * 1.1)
+        for tad_group, num_genes in num_genes_in_TAD.items()
+    }
 
     #samples the expected number of genes from the TAD (or the number of genes if expected is > actual)
-    aggregated_by_TAD['sampled_genes'] = aggregated_by_TAD[aggregated_by_TAD.num_genes_in_TAD > 0].apply(
-        lambda tad : np.random.choice(tad.genes_in_tad, np.min(tad.num_genes_in_TAD, tad.expected_samples), replace = False)
-    )
+    selected_genes = [
+        np.random.choice(genes_in_TAD[tad_group], min(num_genes_in_TAD[tad_group] - 1, num_expected))
+        for tad_group, num_expected in expected_samples.items()
+    ]
+
     #flatten the list to yield background gene selections
-    return set([gene_name for gene_list in aggregated_by_TAD.sampled_genes.values for gene_name in gene_list])
+    return set([gene_name for gene_list in selected_genes for gene in gene_list])
 
 
 #Randomly sample background genes from the whole genome
@@ -55,27 +59,28 @@ def random_background_genes(background_gene_list, num_selected = 3000):
 
 
 #recieve user gene-of-interest list, process text, validate, and convert to gene symbols
-def process_user_genelist(user_genelist, gene_metadata)
+def process_user_genelist(user_genelist, gene_symbols, gene_refseqIDs):
+    """
+    user_genelist: list of user-supplied text, supposedly containing some gene-like entries
+    gene_symbols: list of gene symbols
+    gene_refseqIDs: list of refseqIDs corresponding to their respective gene symbol
+    """
+    gene_symbols, gene_refseqIDs = np.array(gene_symbols), np.array(gene_refseqIDs)
 
-    supplied_symbols = gene_metadata.geneName.isin(user_genelist)
-    supplied_ids = gene_metadata.refseqID.isin(user_genelist)
+    supplied_symbols = np.isin(gene_symbols, user_genelist)
+    supplied_ids = np.isin(gene_refseqIDs, user_genelist)
 
     user_supplied_genes = np.logical_and(supplied_symbols, supplied_ids)
 
-    return list(gene_metadata[user_supplied_genes].geneName.values)
+    return list(gene_symbols[user_supplied_genes].values)
 
 #creates a label vector, indexed by gene symbol, with binary labels: {0 = background, 1 = query}
 def create_label_vector(query_list, background_list):
-    return pd.Series(
-        index = query_list + background_list,
-        values = np.concatenate(
-            [np.ones(len(query_list)), np.zeros(len(background_list))]
-        )
-    )
+    return query_list + background_list, np.concatenate([np.ones(len(query_list)), np.zeros(len(background_list))])
 
 
 #converts user-entered data to two gene lists: a query list, and a background list
-def process_gene_lists(query_genes, gene_metadata, TAD_data, num_selected = 3000, user_background_genes = None, method = 'TAD'):
+def process_gene_lists(query_genes, gene_symbols, gene_refseqIDs, TAD_data, num_selected = 3000, user_background_genes = None, method = 'TAD'):
     '''
     query_genes: text-processed list of symbols or refseqIDs (or both) that user supplies
     gene_metdata: dataframe of chr, start, end, refseq, symbol
@@ -86,16 +91,16 @@ def process_gene_lists(query_genes, gene_metadata, TAD_data, num_selected = 3000
     assert(method == 'TAD' or method == 'random'), 'Method may be either "TAD" or "random". User supplied {}'.format(method)
 
     #process and validate user-supplied query
-    query_list = process_user_genelist(query_genes, gene_metadata)
+    query_list = process_user_genelist(query_genes, gene_symbols, gene_refseqIDs)
 
     num_genes_supplied = len(query_list)
-    assert(20 <= num_genes_supplied <= 200), 'User must provide list of between 20 and 200 genes. Provided {}'.format(int(user_supplied_genes.sum()))
+    assert(20 <= num_genes_supplied <= 200), 'User must provide list of between 20 and 200 genes. Provided {}'.format(int(num_genes_supplied))
 
     #if the user supplied a background candidates list, process that, otherwise take the background candidates to be all genes not in query
     if not user_background_genes is None:
-        background_candidates = process_user_genelist(user_background_genes, gene_metadata)
+        background_candidates = process_user_genelist(user_background_genes, gene_symbols, gene_refseqIDs)
     else:
-        background_candidates = list(gene_metadata[ ~gene_metadata.geneName.isin(query_list)].geneName.values)
+        background_candidates = set(gene_symbols).difference(query_list)
 
     assert(len(background_candidates) >= num_selected), 'Number of background candidates must be greater than or equal number of genes to select as background.'
 
